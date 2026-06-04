16a2081a1eee39862b9efb35f60fe4a2ceab1592
girishHD <girish@example.com>
2026-06-04 21:53:54 +0530

    fix: uncomment anagram check logic in Anagram.js

--- chapter_13_Strings/Anagram.js
+++ chapter_13_Strings/Anagram.js
@@ -3,13 +3,12 @@
 let str1 = "Eat";
 let str2 = "Tea";
 
-let a = str1.toLowerCase().split('').sort().join('');
-// let b = str2.toLowerCase().sort();
+const a = str1.toLowerCase().split("").sort().join();
+const b = str2.toLowerCase().split("").sort().join();
 
-console.log(a.sort);
+if (a === b) {
 
-// if (a === b) {
-//     console.log(str1 + " & " + str2 + " -> Anagram");
-// } else {
-//     console.log(str1 + " & " + str2 + " -> Not Anagram");
-// }
+    console.log(str1 + " & " + str2 + " -> Anagram");
+} else {
+    console.log(str1 + " & " + str2 + " -> Not Anagram");
+}
