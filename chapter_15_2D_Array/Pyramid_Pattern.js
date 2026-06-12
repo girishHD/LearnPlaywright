5de2b3e6402f90301adc506c2de54ba932300ef7
girishHD <girish@example.com>
2026-06-12 07:04:19 +0530

    fix: improve Pyramid_Pattern.js output using row variable

--- chapter_15_2D_Array/Pyramid_Pattern.js
+++ chapter_15_2D_Array/Pyramid_Pattern.js
@@ -5,15 +5,12 @@
 let n = 3;
 
 for (let i = 1; i <= n; i++) {
-    // let row = "";
+    let row = "";
     for (let j = 1; j < n; j++) {
-        // row += " ";
-        process.stdout.write(" ");
+        row += " ";
     }
     for (let k = 1; k <= (2 * i - 1); k++) {
-        // row += "*";
-        process.stdout.write("*");
+        row += "*";
     }
-    // console.log(row.trim());
-    console.log();
+    console.log(row.trim());
 }
